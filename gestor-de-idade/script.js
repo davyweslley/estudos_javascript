406c884a1e494903fcfb17c3890639dd9673babf
Davy Weslley B. Valmorbida <davyweslley@hotmail.com>
2026-05-13 12:34:58 -0300

    Add age verification and gender-based image display
    The script calculates age based on user input and displays corresponding images and messages based on gender.

---
+++ gestor-de-idade/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else { 
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade < 10){
+                // Criança
+                img.setAttribute('src', 'img/homem-bb.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/homem-jovem.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'img/homem.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'img/homem-idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade < 10){
+                // Criança
+                img.setAttribute('src', 'img/mulher-bb.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/mulher-jovem.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'img/mulher.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'img/mulher-idosa.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
